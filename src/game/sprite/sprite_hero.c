7743edbe925ef743ce206a52bfffedc8d1abb28c
andy <aksommerville@gmail.com>
2025-10-11 06:55:26 -0400

    action triggers

--- src/game/sprite/sprite_hero.c
+++ src/game/sprite/sprite_hero.c
@@ -2,6 +2,22 @@
 
 #define WALK_SPEED 6.000
 #define CORRECTION_SPEED 1.000
+#define BOOMERANG_DOUBLE_CLICK_TIME 0.250 /* 0.200..0.250 feels about right */
+#define WAND_HOLD_TIME 0.500
+#define VAULT_WARMUP_TIME 0.250
+#define VAULT_TIME 0.250
+#define VAULT_SPEED 10.000
+#define SWING_MINIMUM_TIME 0.250
+#define HERO_EVENT_LIMIT 16
+
+#define ACTION_STATE_NONE 0
+#define ACTION_STATE_STAB 1
+#define ACTION_STATE_SWING 2
+#define ACTION_STATE_VAULT 3
+#define ACTION_STATE_BOOMERANG 4
+#define ACTION_STATE_WAND 5
+#define ACTION_STATE_SLINGSHOT 6
+#define ACTION_STATE_LOCKPICK 7
 
 struct sprite_hero {
   struct sprite hdr;
@@ -10,7 +26,19 @@ struct sprite_hero {
   uint8_t facedir; // 0x40,0x10,0x08,0x02
   double animclock;
   int animframe;
+  double dumbclock; // Counts up always.
+  struct hero_event {
+    int press,release;
+    double time;
+  } eventv[HERO_EVENT_LIMIT];
+  int eventp;
+  int fresh_event; // Nonzero for one update after a new event is pushed.
+  int action_state;
+  double actionclock;
+  int vaultdx,vaultdy;
 };
+
+static struct hero_event empty_event={0};
 
 #define SPRITE ((struct sprite_hero*)sprite)
 
@@ -30,10 +58,264 @@ static int _hero_init(struct sprite *sprite) {
   return 0;
 }
 
+/* Get buttons pressed and released at some point in our recent past.
+ * (dp) in (-HERO_EVENT_LIMIT..-1) normally, and -1 is the most recent change.
+ * You can also use (0..HERO_EVENT_LIMIT-1) to read from the beginning of history forward.
+ * (not sure we'd ever want to do that).
+ */
+
+static struct hero_event *hero_get_event(struct sprite *sprite,int dp) {
+  if ((dp<-HERO_EVENT_LIMIT)||(dp>=HERO_EVENT_LIMIT)) return &empty_event;
+  int p=SPRITE->eventp+dp;
+  if (p<0) p+=HERO_EVENT_LIMIT;
+  else if (p>=HERO_EVENT_LIMIT) p-=HERO_EVENT_LIMIT;
+  return SPRITE->eventv+p;
+}
+
+/* If current context permits a lockpick operation, start it and return nonzero.
+ */
+ 
+static int hero_check_lockpick(struct sprite *sprite) {
+  int x=SPRITE->qx,y=SPRITE->qy;
+  switch (SPRITE->facedir) {
+    case 0x40: y--; break;
+    case 0x10: x--; break;
+    case 0x08: x++; break;
+    case 0x02: y++; break;
+  }
+  if ((x<0)||(y<0)||(x>=NS_sys_mapw)||(y>=NS_sys_maph)) return 0;
+  //TODO Check poi for an unpicked lock at (x,y). If one exists, enter the lockpick modal and return nonzero.
+  return 0;
+}
+
+/* If history indicates a boomerang, start it and return nonzero.
+ */
+ 
+static int hero_check_boomerang(struct sprite *sprite) {
+  // Must be exactly [press,release,press] of SOUTH, within a time threshold.
+  const struct hero_event *press1=hero_get_event(sprite,-3);
+  const struct hero_event *release=hero_get_event(sprite,-2);
+  const struct hero_event *press2=hero_get_event(sprite,-1);
+  if ((press1->press!=EGG_BTN_SOUTH)||press1->release) return 0;
+  if ((release->release!=EGG_BTN_SOUTH)||release->press) return 0;
+  if ((press2->press!=EGG_BTN_SOUTH)||press2->release) return 0;
+  double duration=press2->time-press1->time;
+  if (duration>BOOMERANG_DOUBLE_CLICK_TIME) return 0;
+  // OK, we're doing it.
+  fprintf(stderr,"BOOMERANG\n");//TODO
+  SPRITE->action_state=ACTION_STATE_BOOMERANG;
+  return 1;
+}
+
+/* With STAB or SWING ongoing, check whether to enter WAND mode.
+ */
+ 
+static int hero_check_wand(struct sprite *sprite) {
+  // Must be no intervening events (even inert-feeling stuff like releasing the dpad).
+  const struct hero_event *event=hero_get_event(sprite,-1);
+  if ((event->press!=EGG_BTN_SOUTH)||event->release) return 0;
+  double duration=SPRITE->dumbclock-event->time;
+  if (duration<WAND_HOLD_TIME) return 0;
+  // OK, we're doing it.
+  fprintf(stderr,"WAND\n");
+  SPRITE->action_state=ACTION_STATE_WAND;
+  return 1;
+}
+
+/* If VAULT should begin, do it and return nonzero.
+ */
+ 
+static int hero_check_vault(struct sprite *sprite) {
+  // Vault can only be done in cardinal directions, and must be exactly [dpad,south] with a certain minimum interval between.
+  const struct hero_event *dpad=hero_get_event(sprite,-2);
+  const struct hero_event *south=hero_get_event(sprite,-1);
+  if ((south->press!=EGG_BTN_SOUTH)||south->release) return 0;
+  if (dpad->release) return 0;
+  double duration=south->time-dpad->time;
+  if (duration<VAULT_WARMUP_TIME) return 0;
+  int dx=0,dy=0;
+  switch (dpad->press) {
+    case EGG_BTN_LEFT: dx=-1; break;
+    case EGG_BTN_RIGHT: dx=1; break;
+    case EGG_BTN_UP: dy=-1; break;
+    case EGG_BTN_DOWN: dy=1; break;
+    default: return 0;
+  }
+  // OK, we're doing it.
+  fprintf(stderr,"VAULT %+d,%+d\n",dx,dy);
+  SPRITE->action_state=ACTION_STATE_VAULT;
+  SPRITE->vaultdx=dx;
+  SPRITE->vaultdy=dy;
+  SPRITE->actionclock=VAULT_TIME;
+  return 1;
+}
+
+/* Update during VAULT.
+ */
+ 
+static void hero_update_vault(struct sprite *sprite,double elapsed) {
+  uint32_t pvphymask=sprite->phymask;
+  sprite->phymask&=~(1<<NS_physics_water);
+  if (SPRITE->vaultdx) sprite_move(sprite,SPRITE->vaultdx*VAULT_SPEED*elapsed,0.0);
+  else sprite_move(sprite,0.0,SPRITE->vaultdy*VAULT_SPEED*elapsed);
+  sprite->phymask=pvphymask;
+  if ((SPRITE->actionclock-=elapsed)<=0.0) {
+    SPRITE->action_state=0;
+    //TODO check whether we're over water and either correct position or drown
+  }
+}
+
+/* Update during WAND.
+ */
+ 
+static void hero_append_spell(struct sprite *sprite,int btnid) {
+  fprintf(stderr,"%s 0x%04x\n",__func__,btnid);//TODO
+}
+ 
+static void hero_update_wand(struct sprite *sprite,double elapsed) {
+  if (SPRITE->fresh_event) {
+    const struct hero_event *event=hero_get_event(sprite,-1);
+    if (event->release&EGG_BTN_SOUTH) {
+      fprintf(stderr,"%s:%d: Commit spell or enter SLINGSHOT\n",__FILE__,__LINE__);//TODO
+      SPRITE->action_state=0;
+      return;
+    }
+    if (event->press&EGG_BTN_LEFT) hero_append_spell(sprite,EGG_BTN_LEFT);
+    if (event->press&EGG_BTN_RIGHT) hero_append_spell(sprite,EGG_BTN_RIGHT);
+    if (event->press&EGG_BTN_UP) hero_append_spell(sprite,EGG_BTN_UP);
+    if (event->press&EGG_BTN_DOWN) hero_append_spell(sprite,EGG_BTN_DOWN);
+  }
+}
+
+/* Update during SLINGSHOT.
+ */
+ 
+static void hero_update_slingshot(struct sprite *sprite,double elapsed) {
+  //TODO slingshot
+  SPRITE->action_state=0;
+}
+
+/* Check SWING, begin if warranted and return nonzero.
+ */
+ 
+static int is_single_dpad(int input) {
+  switch (input) {
+    case EGG_BTN_LEFT:
+    case EGG_BTN_RIGHT:
+    case EGG_BTN_UP:
+    case EGG_BTN_DOWN:
+      return 1;
+  }
+  return 0;
+}
+ 
+static int hero_check_swing(struct sprite *sprite) {
+  // Most recent pressed must be [axisA,axisB,south], within some minimum interval. Intervening releases are ok.
+  int dp=-1,got_swing=0,axisb=0,axisa=0;
+  double starttime;
+  for (;dp>=-HERO_EVENT_LIMIT;dp--) {
+    const struct hero_event *event=hero_get_event(sprite,dp);
+    if (!event->press) continue;
+    if (!got_swing) {
+      if (event->press!=EGG_BTN_SOUTH) return 0;
+      got_swing=1;
+    } else if (!axisb) {
+      if (!is_single_dpad(axisb=(event->press&(EGG_BTN_LEFT|EGG_BTN_RIGHT|EGG_BTN_UP|EGG_BTN_DOWN)))) return 0;
+    } else {
+      if (!is_single_dpad(axisa=(event->press&(EGG_BTN_LEFT|EGG_BTN_RIGHT|EGG_BTN_UP|EGG_BTN_DOWN)))) return 0;
+      starttime=event->time;
+      break;
+    }
+  }
+  if (!axisa) return 0;
+  if (axisa&(EGG_BTN_LEFT|EGG_BTN_RIGHT)) {
+    if (axisb&(EGG_BTN_LEFT|EGG_BTN_RIGHT)) return 0; // Nope, both horizontal.
+  } else {
+    if (axisb&(EGG_BTN_UP|EGG_BTN_DOWN)) return 0; // Nope, both vertical.
+  }
+  double duration=SPRITE->dumbclock-starttime;
+  if (duration>SWING_MINIMUM_TIME) return 0;
+  // OK, we're doing it.
+  fprintf(stderr,"SWING from 0x%04x to 0x%04x\n",axisa,axisb);
+  //TODO Whack things.
+  SPRITE->action_state=ACTION_STATE_SWING;
+  return 1;
+}
+
+/* Begin STAB.
+ */
+ 
+static void hero_begin_stab(struct sprite *sprite) {
+  fprintf(stderr,"STAB\n");
+  //TODO Whack things.
+  SPRITE->action_state=ACTION_STATE_STAB;
+}
+
+/* Check for all usage of the wishbone.
+ */
+ 
+static void hero_update_wishbone(struct sprite *sprite,double elapsed) {
+  
+  // If an action is in progress, that takes precedence over everything else.
+  switch (SPRITE->action_state) {
+    // VAULT,WAND,SLINGSHOT: Complex, call out.
+    case ACTION_STATE_VAULT: hero_update_vault(sprite,elapsed); return;
+    case ACTION_STATE_WAND: hero_update_wand(sprite,elapsed); return;
+    case ACTION_STATE_SLINGSHOT: hero_update_slingshot(sprite,elapsed); return;
+    
+    // STAB,SWING: End action on release, or begin WAND if held long enough.
+    // A held stroke is noop, it doesn't continue hurting foes or anything. Only reason to hold is to enter the WAND state.
+    case ACTION_STATE_STAB:
+    case ACTION_STATE_SWING: {
+        if (!(SPRITE->input&EGG_BTN_SOUTH)) {
+          SPRITE->action_state=0;
+        } else {
+          hero_check_wand(sprite);
+        }
+      } return;
+      
+    // BOOMERANG,LOCKPICK: No activity, just wait for the hold to release.
+    case ACTION_STATE_BOOMERANG:
+    case ACTION_STATE_LOCKPICK: {
+        if (!(SPRITE->input&EGG_BTN_SOUTH)) {
+          SPRITE->action_state=0;
+        }
+      } return;
+  }
+  
+  // Nothing in progress. Has something started just now?
+  if (SPRITE->fresh_event) {
+    const struct hero_event *event=hero_get_event(sprite,-1);
+    if (event->press&EGG_BTN_SOUTH) {
+      // The start of a stroke is one of: stab,swing,vault,boomerang,lockpick
+      if (hero_check_lockpick(sprite)) return;
+      if (hero_check_boomerang(sprite)) return;
+      if (hero_check_vault(sprite)) return;
+      if (hero_check_swing(sprite)) return;
+      hero_begin_stab(sprite);
+    }
+  }
+}
+
+/* Is walking suppressed, in the given state?
+ */
+ 
+static int action_state_prevents_walking(int state) {
+  switch (state) {
+    case ACTION_STATE_VAULT:
+    case ACTION_STATE_WAND:
+    case ACTION_STATE_SLINGSHOT:
+      return 1;
+  }
+  return 0;
+}
+
 /* Walking and such.
  */
  
 static void hero_update_walk(struct sprite *sprite,double elapsed) {
+
+  if (action_state_prevents_walking(SPRITE->action_state)) return;
 
   int walkdx=0,walkdy=0,xok=0,yok=0;
   switch (SPRITE->input&(EGG_BTN_LEFT|EGG_BTN_RIGHT)) {
@@ -112,9 +394,14 @@ static void hero_check_quantized_position(struct sprite *sprite) {
  */
  
 static void _hero_update(struct sprite *sprite,double elapsed) {
+  SPRITE->dumbclock+=elapsed;
+  if (g.item==NS_flag_wishbone) {
+    hero_update_wishbone(sprite,elapsed);
+  }
   hero_update_walk(sprite,elapsed);
   hero_update_animation(sprite,elapsed);
   hero_check_quantized_position(sprite);
+  SPRITE->fresh_event=0;
 }
 
 /* Render.
@@ -167,18 +454,26 @@ const struct sprite_type sprite_type_hero={
 void sprite_hero_input(struct sprite *sprite,int input,int pvinput) {
   SPRITE->input=input;
   
+  // All changes to input state get recorded for examination during update.
+  SPRITE->fresh_event=1;
+  struct hero_event *event=SPRITE->eventv+SPRITE->eventp++;
+  if (SPRITE->eventp>=HERO_EVENT_LIMIT) SPRITE->eventp=0;
+  event->press=input&~pvinput;
+  event->release=pvinput&~input;
+  event->time=SPRITE->dumbclock;
+  
   // When a dpad button goes ON, facedir changes immediately.
-  if ((input&EGG_BTN_LEFT)&&!(pvinput&EGG_BTN_LEFT)) SPRITE->facedir=0x10;
-  if ((input&EGG_BTN_RIGHT)&&!(pvinput&EGG_BTN_RIGHT)) SPRITE->facedir=0x08;
-  if ((input&EGG_BTN_UP)&&!(pvinput&EGG_BTN_UP)) SPRITE->facedir=0x40;
-  if ((input&EGG_BTN_DOWN)&&!(pvinput&EGG_BTN_DOWN)) SPRITE->facedir=0x02;
+  if (event->press&EGG_BTN_LEFT) SPRITE->facedir=0x10;
+  if (event->press&EGG_BTN_RIGHT) SPRITE->facedir=0x08;
+  if (event->press&EGG_BTN_UP) SPRITE->facedir=0x40;
+  if (event->press&EGG_BTN_DOWN) SPRITE->facedir=0x02;
   
   // When a dpad button goes OFF, we're facing that way, and one other dpad button is held, face the one still held.
   if (
-    (!(input&EGG_BTN_LEFT)&&(pvinput&EGG_BTN_LEFT)&&(SPRITE->facedir==0x10))||
-    (!(input&EGG_BTN_RIGHT)&&(pvinput&EGG_BTN_RIGHT)&&(SPRITE->facedir==0x08))||
-    (!(input&EGG_BTN_UP)&&(pvinput&EGG_BTN_UP)&&(SPRITE->facedir==0x40))||
-    (!(input&EGG_BTN_DOWN)&&(pvinput&EGG_BTN_DOWN)&&(SPRITE->facedir==0x02))
+    ((event->release&EGG_BTN_LEFT)&&(SPRITE->facedir==0x10))||
+    ((event->release&EGG_BTN_RIGHT)&&(SPRITE->facedir==0x08))||
+    ((event->release&EGG_BTN_UP)&&(SPRITE->facedir==0x40))||
+    ((event->release&EGG_BTN_DOWN)&&(SPRITE->facedir==0x02))
   ) {
     if (input&EGG_BTN_LEFT) SPRITE->facedir=0x10;
     else if (input&EGG_BTN_RIGHT) SPRITE->facedir=0x08;
